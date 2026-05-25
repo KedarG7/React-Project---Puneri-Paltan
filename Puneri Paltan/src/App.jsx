53dd58621b714977216aa139132bc9646f50b28c
KedarG7 <gurav.kedar2005@gmail.com>
2026-05-25 17:32:00 +0530

    partly completing

--- Puneri Paltan/src/App.jsx
+++ Puneri Paltan/src/App.jsx
@@ -7,16 +7,23 @@ import Standings from './assets/pages/Standings/Standings';
 import Fixtures from './assets/pages/Fixtures/Fixtures';
 import PaltanWorld from './assets/pages/Paltan World/PaltanWorld';
 import Tickets from './assets/pages/Tickets/Tickets';
+import Part2 from './assets/components/Part2';
 
 function App() {
   
 
   return (
     <>
-      <Navbar />
+      <div className="bodyy">
+        <Navbar />
+      </div>
+      <div className="part2">
+        <Part2/>
+      </div>
+
       <Routes>
         <Route path="/players" element={<Players />} />
-        <Route path="/standings" element={<Standings/>} />
+        <Route path="/standings" element={<Standings />} />
         <Route path="/fixtures" element={<Fixtures />} />
         <Route path="/paltan-world" element={<PaltanWorld />} />
         <Route path="/tickets" element={<Tickets />} />
---
+++ Puneri Paltan/src/assets/components/Part2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Part2 = () => {
+  return (
+    <>
+    <p className="scroll">SCROLL</p>
+    </>
+  )
+}
+
+export default Part2
